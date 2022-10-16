3f8ff493e96023f905fe1a554b65f8fbe661fe49
Antoine <103137635+Zgruk@users.noreply.github.com>
2022-10-16 21:07:30 +0300

    added abstract ItemBasemodel and Text, File, Image, Video child models

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,5 @@
+from distutils.command.upload import upload
+from enum import auto
 from operator import mod
 from django.db import models
 from users.models import CustomUser
@@ -48,6 +50,37 @@ class Content(models.Model):
                                related_name='contents',
                                on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,
-                                     on_delete=models.CASCADE)
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in': (
+                                                       'text',
+                                                       'videl',
+                                                       'image',
+                                                       'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(CustomUser,
+                              related_name = '%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
